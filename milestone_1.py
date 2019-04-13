497de778c254ef3b8b6151a58fc7188962a33569
salmajaki <salmaa.eljaki@gmail.com>
2019-04-13 22:37:29 +0200

    Adding nim_grammar.g4

--- milestone_1.py
+++ milestone_1.py
@@ -1,259 +1,259 @@
 import argparse
 from antlr4 import *
-from milestone_1Lexer import milestone_1Lexer
-from milestone_1Listener import milestone_1Listener
-from milestone_1Parser import milestone_1Parser
+from nim_grammarLexer import nim_grammarLexer
+from nim_grammarListener import nim_grammarListener
+from nim_grammarParser import nim_grammarParser
 from antlr4.tree.Trees import Trees
 
-output_file = open("milestone_1_result.txt", "w+")
+output_file = open("nim_grammar_result.txt", "w+")
 
 
 def get_token_type(token):
-    if token.type == milestone_1Lexer.STR_LIT:
+    if token.type == nim_grammarLexer.STR_LIT:
         return "STR_LIT"
-    elif token.type == milestone_1Lexer.CHAR_LIT:
+    elif token.type == nim_grammarLexer.CHAR_LIT:
         return "CHAR_LIT"
-    elif token.type == milestone_1Lexer.TRIPLESTR_LIT:
+    elif token.type == nim_grammarLexer.TRIPLESTR_LIT:
         return "TRIPLESTR_LIT"
-    elif token.type == milestone_1Lexer.RSTR_LIT:
+    elif token.type == nim_grammarLexer.RSTR_LIT:
         return "RSTR_LIT"
-    elif token.type == milestone_1Lexer.GENERALIZED_TRIPLESTR_LIT:
+    elif token.type == nim_grammarLexer.GENERALIZED_TRIPLESTR_LIT:
         return "GENERALIZED_TRIPLESTR_LIT"
-    elif token.type == milestone_1Lexer.GENERALIZED_STR_LIT:
+    elif token.type == nim_grammarLexer.GENERALIZED_STR_LIT:
         return "GENERALIZED_STR_LIT"
-    elif token.type == milestone_1Lexer.AND:
+    elif token.type == nim_grammarLexer.AND:
         return "AND"
-    elif token.type == milestone_1Lexer.ADDR:
+    elif token.type == nim_grammarLexer.ADDR:
         return "ADDR"
-    elif token.type == milestone_1Lexer.AS:
+    elif token.type == nim_grammarLexer.AS:
         return "AS"
-    elif token.type == milestone_1Lexer.ASM:
+    elif token.type == nim_grammarLexer.ASM:
         return "ASM"
-    elif token.type == milestone_1Lexer.BIND:
+    elif token.type == nim_grammarLexer.BIND:
         return "BIND"
-    elif token.type == milestone_1Lexer.BLOCK:
+    elif token.type == nim_grammarLexer.BLOCK:
         return "BLOCK"
-    elif token.type == milestone_1Lexer.BREAK:
+    elif token.type == nim_grammarLexer.BREAK:
         return "BREAK"
-    elif token.type == milestone_1Lexer.CASE:
+    elif token.type == nim_grammarLexer.CASE:
         return "CASE"
-    elif token.type == milestone_1Lexer.CAST:
+    elif token.type == nim_grammarLexer.CAST:
         return "CAST"
-    elif token.type == milestone_1Lexer.CONCEPT:
+    elif token.type == nim_grammarLexer.CONCEPT:
         return "CONCEPT"
-    elif token.type == milestone_1Lexer.CONST:
+    elif token.type == nim_grammarLexer.CONST:
         return "CONST"
-    elif token.type == milestone_1Lexer.CONTINUE:
+    elif token.type == nim_grammarLexer.CONTINUE:
         return "CONTINUE"
-    elif token.type == milestone_1Lexer.CONVERTER:
+    elif token.type == nim_grammarLexer.CONVERTER:
         return "CONVERTER"
-    elif token.type == milestone_1Lexer.DEFER:
+    elif token.type == nim_grammarLexer.DEFER:
         return "DEFER"
-    elif token.type == milestone_1Lexer.DISCARD:
+    elif token.type == nim_grammarLexer.DISCARD:
         return "DISCARD"
-    elif token.type == milestone_1Lexer.DISTINCT:
+    elif token.type == nim_grammarLexer.DISTINCT:
         return "DISTINCT"
-    elif token.type == milestone_1Lexer.DIV:
+    elif token.type == nim_grammarLexer.DIV:
         return "DIV"
-    elif token.type == milestone_1Lexer.DO:
+    elif token.type == nim_grammarLexer.DO:
         return "DO"
-    elif token.type == milestone_1Lexer.ELIF:
+    elif token.type == nim_grammarLexer.ELIF:
         return "ELIF"
-    elif token.type == milestone_1Lexer.ELSE:
+    elif token.type == nim_grammarLexer.ELSE:
         return "ELSE"
-    elif token.type == milestone_1Lexer.END:
+    elif token.type == nim_grammarLexer.END:
         return "END"
-    elif token.type == milestone_1Lexer.ENUM:
+    elif token.type == nim_grammarLexer.ENUM:
         return "ENUM"
-    elif token.type == milestone_1Lexer.EXCEPT:
+    elif token.type == nim_grammarLexer.EXCEPT:
         return "EXCEPT"
-    elif token.type == milestone_1Lexer.EXPORT:
+    elif token.type == nim_grammarLexer.EXPORT:
         return "EXPORT"
-    elif token.type == milestone_1Lexer.FINALLY:
+    elif token.type == nim_grammarLexer.FINALLY:
         return "FINALLY"
-    elif token.type == milestone_1Lexer.FOR:
+    elif token.type == nim_grammarLexer.FOR:
         return "FOR"
-    elif token.type == milestone_1Lexer.FROM:
+    elif token.type == nim_grammarLexer.FROM:
         return "FROM"
-    elif token.type == milestone_1Lexer.FUNC:
+    elif token.type == nim_grammarLexer.FUNC:
         return "FUNC"
-    elif token.type == milestone_1Lexer.IF:
+    elif token.type == nim_grammarLexer.IF:
         return "IF"
-    elif token.type == milestone_1Lexer.IMPORT:
+    elif token.type == nim_grammarLexer.IMPORT:
         return "IMPORT"
-    elif token.type == milestone_1Lexer.IN:
+    elif token.type == nim_grammarLexer.IN:
         return "IN"
-    elif token.type == milestone_1Lexer.INCLUDE:
+    elif token.type == nim_grammarLexer.INCLUDE:
         return "INCLUDE"
-    elif token.type == milestone_1Lexer.INTERFACE:
+    elif token.type == nim_grammarLexer.INTERFACE:
         return "INTERFACE"
-    elif token.type == milestone_1Lexer.IS:
+    elif token.type == nim_grammarLexer.IS:
         return "IS"
-    elif token.type == milestone_1Lexer.ISNOT:
+    elif token.type == nim_grammarLexer.ISNOT:
         return "ISNOT"
-    elif token.type == milestone_1Lexer.ITERATOR:
+    elif token.type == nim_grammarLexer.ITERATOR:
         return "ITERATOR"
-    elif token.type == milestone_1Lexer.LET:
+    elif token.type == nim_grammarLexer.LET:
         return "LET"
-    elif token.type == milestone_1Lexer.MACRO:
+    elif token.type == nim_grammarLexer.MACRO:
         return "MACRO"
-    elif token.type == milestone_1Lexer.METHOD:
+    elif token.type == nim_grammarLexer.METHOD:
         return "METHOD"
-    elif token.type == milestone_1Lexer.MIXIN:
+    elif token.type == nim_grammarLexer.MIXIN:
         return "MIXIN"
-    elif token.type == milestone_1Lexer.MOD:
+    elif token.type == nim_grammarLexer.MOD:
         return "MOD"
-    elif token.type == milestone_1Lexer.NIL:
+    elif token.type == nim_grammarLexer.NIL:
         return "NIL"
-    elif token.type == milestone_1Lexer.NOT:
+    elif token.type == nim_grammarLexer.NOT:
         return "NOT"
-    elif token.type == milestone_1Lexer.NOTIN:
+    elif token.type == nim_grammarLexer.NOTIN:
         return "NOTIN"
-    elif token.type == milestone_1Lexer.OBJECT:
+    elif token.type == nim_grammarLexer.OBJECT:
         return "OBJECT"
-    elif token.type == milestone_1Lexer.OF:
+    elif token.type == nim_grammarLexer.OF:
         return "OF"
-    elif token.type == milestone_1Lexer.OR:
+    elif token.type == nim_grammarLexer.OR:
         return "OR"
-    elif token.type == milestone_1Lexer.OUT:
+    elif token.type == nim_grammarLexer.OUT:
         return "OUT"
-    elif token.type == milestone_1Lexer.PROC:
+    elif token.type == nim_grammarLexer.PROC:
         return "PROC"
-    elif token.type == milestone_1Lexer.PTR:
+    elif token.type == nim_grammarLexer.PTR:
         return "PTR"
-    elif token.type == milestone_1Lexer.RAISE:
+    elif token.type == nim_grammarLexer.RAISE:
         return "RAISE"
-    elif token.type == milestone_1Lexer.REF:
+    elif token.type == nim_grammarLexer.REF:
         return "REF"
-    elif token.type == milestone_1Lexer.RETURN:
+    elif token.type == nim_grammarLexer.RETURN:
         return "RETURN"
-    elif token.type == milestone_1Lexer.SHL:
+    elif token.type == nim_grammarLexer.SHL:
         return "SHL"
-    elif token.type == milestone_1Lexer.SHR:
+    elif token.type == nim_grammarLexer.SHR:
         return "SHR"
-    elif token.type == milestone_1Lexer.STATIC:
+    elif token.type == nim_grammarLexer.STATIC:
         return "STATIC"
-    elif token.type == milestone_1Lexer.TEMPLATE:
+    elif token.type == nim_grammarLexer.TEMPLATE:
         return "TEMPLATE"
-    elif token.type == milestone_1Lexer.TRY:
+    elif token.type == nim_grammarLexer.TRY:
         return "TRY"
-    elif token.type == milestone_1Lexer.TUPLE:
+    elif token.type == nim_grammarLexer.TUPLE:
         return "TUPLE"
-    elif token.type == milestone_1Lexer.TYPE:
+    elif token.type == nim_grammarLexer.TYPE:
         return "TYPE"
-    elif token.type == milestone_1Lexer.USING:
+    elif token.type == nim_grammarLexer.USING:
         return "USING"
-    elif token.type == milestone_1Lexer.WHEN:
+    elif token.type == nim_grammarLexer.WHEN:
         return "WHEN"
-    elif token.type == milestone_1Lexer.WHILE:
+    elif token.type == nim_grammarLexer.WHILE:
         return "WHILE"
-    elif token.type == milestone_1Lexer.XOR:
+    elif token.type == nim_grammarLexer.XOR:
         return "XOR"
-    elif token.type == milestone_1Lexer.YIELD:
+    elif token.type == nim_grammarLexer.YIELD:
         return "YIELD"
-    elif token.type == milestone_1Lexer.IDENTIFIER:
+    elif token.type == nim_grammarLexer.IDENTIFIER:
         return "IDENTIFIER"
-    elif token.type == milestone_1Lexer.LETTER:
+    elif token.type == nim_grammarLexer.LETTER:
         return "LETTER"
-    elif token.type == milestone_1Lexer.VARIABLE:
+    elif token.type == nim_grammarLexer.VARIABLE:
         return "VARIABLE"
-    elif token.type == milestone_1Lexer.EQUALS_OPERATOR:
+    elif token.type == nim_grammarLexer.EQUALS_OPERATOR:
         return "EQUALS_OPERATOR"
-    elif token.type == milestone_1Lexer.ADD_OPERATOR:
+    elif token.type == nim_grammarLexer.ADD_OPERATOR:
         return "ADD_OPERATOR"
-    elif token.type == milestone_1Lexer.MUL_OPERATOR:
+    elif token.type == nim_grammarLexer.MUL_OPERATOR:
         return "MUL_OPERATOR"
-    elif token.type == milestone_1Lexer.MINUS_OPERATOR:
+    elif token.type == nim_grammarLexer.MINUS_OPERATOR:
         return "MINUS_OPERATOR"
-    elif token.type == milestone_1Lexer.DIV_OPERATOR:
+    elif token.type == nim_grammarLexer.DIV_OPERATOR:
         return "DIV_OPERATOR"
-    elif token.type == milestone_1Lexer.LESS_THAN:
+    elif token.type == nim_grammarLexer.LESS_THAN:
         return "LESS_THAN"
-    elif token.type == milestone_1Lexer.GREATER_THAN:
+    elif token.type == nim_grammarLexer.GREATER_THAN:
         return "GREATER_THAN"
-    elif token.type == milestone_1Lexer.OR_OPERATOR:
+    elif token.type == nim_grammarLexer.OR_OPERATOR:
         return "OR_OPERATOR"
-    elif token.type == milestone_1Lexer.AND_OPERATOR:
+    elif token.type == nim_grammarLexer.AND_OPERATOR:
         return "AND_OPERATOR"
-    elif token.type == milestone_1Lexer.BITWISE_NOT_OPERATOR:
+    elif token.type == nim_grammarLexer.BITWISE_NOT_OPERATOR:
         return "BITWISE_NOT_OPERATOR"
-    elif token.type == milestone_1Lexer.NOT_OPERATOR:
+    elif token.type == nim_grammarLexer.NOT_OPERATOR:
         return "NOT_OPERATOR"
-    elif token.type == milestone_1Lexer.XOR_OPERATOR:
+    elif token.type == nim_grammarLexer.XOR_OPERATOR:
         return "XOR_OPERATOR"
-    elif token.type == milestone_1Lexer.AT:
+    elif token.type == nim_grammarLexer.AT:
         return "AT"
-    elif token.type == milestone_1Lexer.MODULUS:
+    elif token.type == nim_grammarLexer.MODULUS:
         return "MODULUS"
-    elif token.type == milestone_1Lexer.DOT:
+    elif token.type == nim_grammarLexer.DOT:
         return "DOT"
-    elif token.type == milestone_1Lexer.COLON:
+    elif token.type == nim_grammarLexer.COLON:
         return "COLON"
-    elif token.type == milestone_1Lexer.SEMI_COLON:
+    elif token.type == nim_grammarLexer.SEMI_COLON:
         return "SEMI_COLON"
-    elif token.type == milestone_1Lexer.COMMA:
+    elif token.type == nim_grammarLexer.COMMA:
         return "COMMA"
-    elif token.type == milestone_1Lexer.OPEN_PAREN:
+    elif token.type == nim_grammarLexer.OPEN_PAREN:
         return "OPEN_PAREN"
-    elif token.type == milestone_1Lexer.CLOSE_PAREN:
+    elif token.type == nim_grammarLexer.CLOSE_PAREN:
         return "CLOSE_PAREN"
-    elif token.type == milestone_1Lexer.OPEN_BRACE:
+    elif token.type == nim_grammarLexer.OPEN_BRACE:
         return "OPEN_BRACE"
-    elif token.type == milestone_1Lexer.CLOSE_BRACE:
+    elif token.type == nim_grammarLexer.CLOSE_BRACE:
         return "CLOSE_BRACE"
-    elif token.type == milestone_1Lexer.OPEN_BRACK:
+    elif token.type == nim_grammarLexer.OPEN_BRACK:
         return "OPEN_BRACK"
-    elif token.type == milestone_1Lexer.CLOSE_BRACK:
+    elif token.type == nim_grammarLexer.CLOSE_BRACK:
         return "CLOSE_BRACK"
-    elif token.type == milestone_1Lexer.HEXDIGIT:
+    elif token.type == nim_grammarLexer.HEXDIGIT:
         return "HEXDIGIT"
-    elif token.type == milestone_1Lexer.OCTDIGIT:
+    elif token.type == nim_grammarLexer.OCTDIGIT:
         return "OCTDIGIT"
-    elif token.type == milestone_1Lexer.BINDIGIT:
+    elif token.type == nim_grammarLexer.BINDIGIT:
         return "BINDIGIT"
-    elif token.type == milestone_1Lexer.HEX_LIT:
+    elif token.type == nim_grammarLexer.HEX_LIT:
         return "HEX_LIT"
-    elif token.type == milestone_1Lexer.DEC_LIT:
+    elif token.type == nim_grammarLexer.DEC_LIT:
         return "DEC_LIT"
-    elif token.type == milestone_1Lexer.OCT_LIT:
+    elif token.type == nim_grammarLexer.OCT_LIT:
         return "OCT_LIT"
-    elif token.type == milestone_1Lexer.BIN_LIT:
+    elif token.type == nim_grammarLexer.BIN_LIT:
         return "BIN_LIT"
-    elif token.type == milestone_1Lexer.DIGIT:
+    elif token.type == nim_grammarLexer.DIGIT:
         return "DIGIT"
-    elif token.type == milestone_1Lexer.INT_LIT:
+    elif token.type == nim_grammarLexer.INT_LIT:
         return "INT_LIT"
-    elif token.type == milestone_1Lexer.INT8_LIT:
+    elif token.type == nim_grammarLexer.INT8_LIT:
         return "INT8_LIT"
-    elif token.type == milestone_1Lexer.INT16_LIT:
+    elif token.type == nim_grammarLexer.INT16_LIT:
         return "INT16_LIT"
-    elif token.type == milestone_1Lexer.INT32_LIT:
+    elif token.type == nim_grammarLexer.INT32_LIT:
         return "INT32_LIT"
-    elif token.type == milestone_1Lexer.INT64_LIT:
+    elif token.type == nim_grammarLexer.INT64_LIT:
         return "INT64_LIT"
-    elif token.type == milestone_1Lexer.UINT_LIT:
+    elif token.type == nim_grammarLexer.UINT_LIT:
         return "UINT_LIT"
-    elif token.type == milestone_1Lexer.UINT8_LIT:
+    elif token.type == nim_grammarLexer.UINT8_LIT:
         return "UINT8_LIT"
-    elif token.type == milestone_1Lexer.UINT16_LIT:
+    elif token.type == nim_grammarLexer.UINT16_LIT:
         return "UINT16_LIT"
-    elif token.type == milestone_1Lexer.UINT32_LIT:
+    elif token.type == nim_grammarLexer.UINT32_LIT:
         return "UINT32_LIT"
-    elif token.type == milestone_1Lexer.UINT64_LIT:
+    elif token.type == nim_grammarLexer.UINT64_LIT:
         return "UINT64_LIT"
-    elif token.type == milestone_1Lexer.EXP:
+    elif token.type == nim_grammarLexer.EXP:
         return "EXP"
-    elif token.type == milestone_1Lexer.FLOAT_LIT:
+    elif token.type == nim_grammarLexer.FLOAT_LIT:
         return "FLOAT_LIT"
-    elif token.type == milestone_1Lexer.FLOAT32_SUFFIX:
+    elif token.type == nim_grammarLexer.FLOAT32_SUFFIX:
         return "FLOAT32_SUFFIX"
-    elif token.type == milestone_1Lexer.FLOAT32_LIT:
+    elif token.type == nim_grammarLexer.FLOAT32_LIT:
         return "FLOAT32_LIT"
-    elif token.type == milestone_1Lexer.FLOAT64_SUFFIX:
+    elif token.type == nim_grammarLexer.FLOAT64_SUFFIX:
         return "FLOAT64_SUFFIX"
-    elif token.type == milestone_1Lexer.FLOAT64_LIT:
+    elif token.type == nim_grammarLexer.FLOAT64_LIT:
         return "FLOAT64_LIT"
-    elif token.type == milestone_1Lexer.INDENT:
+    elif token.type == nim_grammarLexer.INDENT:
         return "INDENT"
     else:
         return "ERROR UNKNOWN TOKEN"
@@ -263,9 +263,9 @@ def main():
     with open(args.file, "r") as file:
         lines = file.read()
     input_stream = InputStream(lines)
-    lexer = milestone_1Lexer(input_stream)
+    lexer = nim_grammarLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
-    parser = milestone_1Parser(token_stream)
+    parser = nim_grammarParser(token_stream)
 
     #   tree = parser.start()
     #   print(Trees.toStringTree(tree,None, parser))
---
+++ nim_grammar.g4
@@ -0,0 +1,5 @@
+grammar nim_grammar;
+
+import milestone_1;
+
+start : (expr)*;
